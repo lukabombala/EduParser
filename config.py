c1aa150f82ca46526770a7a58dd054d1ac14b239
lukabombala <14.1996Impeesateam>
2022-03-04 20:20:25 +0100

    added example config file

---
+++ config.py
@@ -0,0 +1,6 @@
+# test config file
+# to main program to work it is needed to enter correct password and username
+username = 'enter-username'
+password = 'enter-password'
+background = True
+database_name = 'test.db'
