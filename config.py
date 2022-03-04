97d8d072840845e3cb6c7f38405b5cb2bb7f4968
Lukasz <43553800+lukabombala@users.noreply.github.com>
2022-03-04 22:32:01 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+# test config file
+# to main program to work it is needed to enter correct password and username for EdukacjaCL and gmail account
+
+# credentials
+username = 'enter-username'
+password = 'enter-password'
+email = 'enter-email'
+email_password = 'enter-password'
+target_email = 'enter-email'
+
+# other settings
+
+background = True
+database_name = 'test.db'
+stop_value = 10
